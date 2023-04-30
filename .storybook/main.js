8c5257b7ba709b559b8026ac8746a100e86cb352
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2023-04-30 11:57:24 -0300

    feat: update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,36 @@
+import remarkGfm from 'remark-gfm'
+
 module.exports = {
   stories: ['../src/components/**/*.stories.tsx'],
-  addons: ['@storybook/addon-essentials'],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5'
+  addons: [
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm]
+          }
+        }
+      }
+    }
+  ],
+  webpackFinal: async (config) => {
+    config.module.rules[0].exclude = /node_modules/
+    return config
   },
-  staticDirs: ['../public']
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
+  },
+  staticDirs: ['../public'],
+  docs: {
+    autodocs: true
+  },
+  core: {
+    builder: 'webpack5'
+  }
 }
